3b9c358d4f693ae47bfb7f4d4beb2ba8b057d64c
UAhmetova <uliaahm.com@yandex.ru>
2026-08-12 14:03:56 +0300

    Добавление реализации подсчета количества статистики за месяц

--- src/MonthData.kt
+++ src/MonthData.kt
@@ -1,3 +1,31 @@
 class MonthData {
     val days = MutableList(30) { 0 }
+
+    fun printDaysAndStepsFromMonth() {
+        println("Количество пройденных шагов по дням: ")
+
+        days.forEachIndexed { index, steps ->
+            println("${index + 1} день: $steps")
+        }
+    }
+
+    fun sumStepsFromMonth() :Int = days.sum()
+
+    fun maxSteps() :Int = days.maxOrNull() ?: 0
+
+    fun bestSeries(goalByStepsPerDay :Int) :Int {
+        var count = 0
+        var maxCount = 0
+
+        days.forEach { steps ->
+            if (steps >= goalByStepsPerDay) {
+                count++
+                maxCount = maxOf(maxCount, count)
+            } else {
+                count = 0
+            }
+        }
+
+        return maxCount
+    }
 }
